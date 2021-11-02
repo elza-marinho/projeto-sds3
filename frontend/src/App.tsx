a3e608afb40d9983e688c4b14066e2dd0497f503
Elza Marinho <elza.cristinam@gmail.com>
2021-11-02 18:06:37 -0300

    basic components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import DataTable from "components/DataTable";
+import NavBar from "components/NavBar";
+import Footer from "components/NavBar/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Hello</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
